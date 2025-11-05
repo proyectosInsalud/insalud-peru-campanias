91e5464578fcffb369259d67c5bf53fd97c9dde1
LuxBlas <practicante.frontend@insalud.pe>
2025-11-05 13:37:48 -0500

    feat: Change of number and email on the south/vph page

--- src/data/sedes/sur.ts
+++ src/data/sedes/sur.ts
@@ -3,7 +3,7 @@ export const surData = {
     city: "Sur", 
     country: "Perú",
     address: "Av. José C. Mariátegui Mz J • Lote 27, San Juan de Miraflores 15801",
-    phone: "+51 957 016 498",
+    phone: "+51 961 724 899",
     email: "contacto.citas@insalud.pe", 
     socials: {
       instagram: "https://www.instagram.com/insalud.sedequito?igsh=YThkMDh3eXQ5d3Q4",
@@ -13,8 +13,8 @@ export const surData = {
     landings: {
       vph: {
         gestor: "Sheyla",
-        email: "gestor.citas10@insalud.pe",
-        whatsapp: "957016498",
+        email: "gestor.citas11@insalud.pe",
+        whatsapp: "961724899",
         message: "¡Hola! Vi su landing de tratamiento para eliminación de verrugas por VPH en su sede Sur. ¿Podrían darme más información sobre el procedimiento y agendar una cita?"
       }
     },
